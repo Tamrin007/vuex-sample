da5608e978228e6120112f7177a3b82ce5c24499
Tamrin007 <tamrin.bitterchococo@gmail.com>
2017-09-06 16:34:08 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <vue-header></vue-header>
+  <time-line></time-line>
+</div>
+</template>
+
+<script>
+import VueHeader from './components/Header'
+import TimeLine from './components/TimeLine'
+
+export default {
+  components: {
+    VueHeader,
+    TimeLine
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import store from './vuex/store'
+Vue.use(Vuex)
+
+/* eslint-disable no-new */
+import App from './App'
+
+new Vue({
+  store,
+  el: '#app',
+  components: {
+    App
+  }
+})
